88644cf556725267cee6f2525d08253681bd5a4c
AbuAlSaeidRajon <khanrajon5@gmail.com>
2025-01-19 19:42:18 +0200

    exercise 3 done

--- Week_1/Operators/operator_03.js
+++ Week_1/Operators/operator_03.js
@@ -5,3 +5,15 @@ Calculate the total number of fruits by adding apples and bananas, and store the
 Use console.log() to print: "We have 25 fruits in total."
 */
 
+// Create variables for apples, bananas, and totalFruits
+let apples = 10;
+let bananas = 15;
+
+// Calculate the total number of fruits
+let totalFruits = apples + bananas;
+
+// Print the total number of fruits
+console.log("We have " + totalFruits + " fruits in total.");
+
+// we can also show print the output by using `´...........
+// console.log(`We have ${totalFruits} fruits in total.`);
