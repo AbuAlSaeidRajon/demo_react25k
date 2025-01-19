66f3ca9a9e045be251fac9e411543e5984f9a88a
AbuAlSaeidRajon <khanrajon5@gmail.com>
2025-01-19 19:53:20 +0200

    exercise of varibales 3 done

--- Week_1/Variables/variables_03.js
+++ Week_1/Variables/variables_03.js
@@ -5,3 +5,11 @@ Create a new variable named description and combine animalType and animalName in
 Use console.log() to print the value of description. 
 */
 
+let animalType = "Cat";
+let animalName = "Whiskers";
+
+// Create a new variable named description and combine the values into a sentence
+let description = "The " + animalType + " is named " + animalName + ".";
+
+// Print the value of description
+console.log(description);
