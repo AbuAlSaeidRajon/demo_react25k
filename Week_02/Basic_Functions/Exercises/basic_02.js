2d923df7029bc7f341ca0af26b5427eadc677901
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-01-27 10:04:13 +0200

    basic exercise 02 complete

--- Week_02/Basic_Functions/Exercises/basic_02.js
+++ Week_02/Basic_Functions/Exercises/basic_02.js
@@ -7,3 +7,11 @@ Print the results using console.log().
 Bonus: Modify the function to use implicit return. 
 */
 
+const isEven = (num) => {
+    const result = (num) % 2;
+    console.log("Result: ". result);
+    return num % 2 === 0;
+};
+
+console.log(isEven(4));
+console.log(isEven(7));
